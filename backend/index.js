59483e1f2bf3f8f447d41a606112fc1a3195c9b0
Deepakdekisugi <dk376907@gmail.com>
2025-04-03 19:32:51 +0530

    added basic functionality in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,26 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
+app.get("/home", (req,res)=>{
+    return res.status(200).json({
+        message: "I m coming form backend",
+        success: true
+    })
+})
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
 
 // Routes
 app.get('/', (req, res) => {
